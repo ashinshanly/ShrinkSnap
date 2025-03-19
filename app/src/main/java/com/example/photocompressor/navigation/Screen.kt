dd563a81f3c358ad8e97e7dca7cddd21cec0321a
kshanly <k.s.ashin.shanly@oracle.com>
2025-03-19 23:00:47 +0530

    Animations

--- app/src/main/java/com/example/photocompressor/navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.photocompressor.navigation
-
-sealed class Screen(val route: String) {
-    object Home : Screen("home")
-    object Compress : Screen("compress")
-    object Results : Screen("results")
-    object Settings : Screen("settings")
-} 
